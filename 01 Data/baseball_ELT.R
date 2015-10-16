0119278725f447d2e973bb383689c8b7afc885dd
Bryan A <bca625@gmail.com>
2015-10-16 12:56:19 -0500

    asdf

---
+++ 01 Data/baseball_ELT.R
@@ -0,0 +1,64 @@
+setwd("~/Assignments/UT Austin/Data Vis/DV_RProject3/01 Data") ###
+
+file_path <- "baseball.csv"
+
+baseball <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(baseball) <- gsub("\\.+", "_", names(baseball))
+
+str(baseball) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+measures <- c("Rk","Age","G","PA","AB","R","H","doubles","triples","HR","RBI","SB","CS","BB","SO","BA",	"OBP","SLG","OPS","TB","GDP","HBP","SH","SF","IBB")
+#measures <- NA # Do this if there are no measures.
+
+# Get rid of special characters in each column.
+# Google ASCII Table to understand the following:
+for(n in names(baseball)) {
+  baseball[n] <- data.frame(lapply(baseball[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+
+dimensions <- setdiff(names(baseball), measures)
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    # Get rid of " and ' in dimensions.
+    baseball[d] <- data.frame(lapply(baseball[d], gsub, pattern="[\"']",replacement= ""))
+    # Change & to and in dimensions.
+    baseball[d] <- data.frame(lapply(baseball[d], gsub, pattern="&",replacement= " and "))
+    # Change : to ; in dimensions.
+    baseball[d] <- data.frame(lapply(baseball[d], gsub, pattern=":",replacement= ";"))
+  }
+}
+
+library(lubridate)
+# Fix date columns, this needs to be done by hand because | needs to be correct.
+#                                                        \_/
+
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# baseball["State"] <- data.frame(lapply(baseball["State"], toupper))
+
+#Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    baseball[m] <- data.frame(lapply(baseball[m], gsub, pattern="[^--.0-9]",replacement= ""))
+  }
+}
+
+write.csv(baseball, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
---
+++ 02 Data Wrangling/baseball_wrangling.R
@@ -0,0 +1,14 @@
+require(tidyr)
+require(dplyr)
+require("jsonlite")
+require("RCurl")
+require("reshape2")
+require("ggplot2")
+
+
+baseball <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from baseball"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+tbl_df(baseball)
+View(baseball)
+
+#df <- rename(baseball, tbl = table) # table is a reserved word in Oracle so rename it to tbl.
